c92de4613fcc0a0bdc4affaac22a85f92969f8d8
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2022-04-23 09:44:01 -0500

    Variables y su configuracion de scope

--- main.js
+++ main.js
@@ -1,25 +1,8 @@
-console.log(`Hola mundo :v`);
-console.error(`Miguel Angel`);
-console.warn(`Hola Estudiantes`);
-console.info(`informacion`);
 
 
-console.log(`%cMensaje personalizado`,`font-family:cursive; color:green; background-color:#7979794d;`);
+debugger;
+let nombre = `Miguel`; //Inicalizar la variable
+var edad; //Declarar variables
 
-console.group(`Diplomado de JavaScript`);
-    console.log(`Miguel`);
-    console.log(`Mercedes`);
-    console.log(`Oscar`);
-    console.log(`Alonso`);
-    console.log(`Jose`);
-console.groupEnd();
+console.log(window);
 
-console.table([`Alonso`]);
-console.table({Nombre:`Alonso`});
-
-console.table([{Nombre:`Alonso`, apellido: `Leon`}]);
-console.time();
-    console.count();
-    console.count();
-    console.count();
-console.timeLog();
